d309aa031c2ea4eaccdd2fba7cf58d328ba14082
Alex Bussey <awbussey83@gmail.com>
2018-06-11 22:38:13 -0400

    Jun 11 2018

--- public/script.js
+++ public/script.js
@@ -1,1 +1,16 @@
-console.log('It works.');
+new Vue({
+  el: '#app',
+  data: {
+    total: 0,
+    items: [
+      { title: 'Item 1' },
+      { title: 'Item 2' },
+      { title: 'Item 3' }
+    ]
+  },
+  methods: {
+    addItem: function() {
+      this.total += 9.99;
+    }
+  }
+});
